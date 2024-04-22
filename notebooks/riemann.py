3a663b95295e8f1ea9f58ee1580f1c88af852b12
Pierre Labadens <labadens.pierre@gmail.com>
2024-04-22 14:06:00 +0200

    riemann: first pass at 2d model

--- notebooks/riemann.py
+++ notebooks/riemann.py
@@ -5,130 +5,110 @@ import numpy as onp
 
 from jax import Array, jit, tree_util
 from jax.typing import ArrayLike
-
-
-def coordinates(n, Lbox=2.0 * jnp.pi) -> tuple[float, Array]:
-    ds = Lbox / n
-    # Two ways of accomplishing the same thing, either with arange or linspace
-    # x = np.linspace(0., Lbox, num=n, endpoint=False)+0.5*ds
-    x = ds * (jnp.arange(n) + 0.5)
-    return ds, x
+from typing import Optional
 
 
 # Compute a "left slope". Returns the slope at the cell interface between x_i-1/2 and x_i+1/2
 # at the index position i. Left slope is useful for computing slopes
 @jit
-def left_slope(f: ArrayLike, axis=0):
-    return f - jnp.roll(f, 1, axis)
+def left_slope(function_values: ArrayLike, axis=0):
+    return function_values - jnp.roll(function_values, 1, axis)
 
 
-# Slopes are calculated at the grid center position
-# Four slopes are given:
-#   no_slope, Cen, MinMod, MonCen
-#############################################################
-# no_slope is zero everywhere producing the Upwind method
 @jit
-def no_slope(f):
-    return jnp.zeros_like(f)
+def monotonized_central(function_values) -> tuple[Array]:
+    """Monotonized central slope limiter"""
 
+    if function_values.ndim == 1:
+        ls = left_slope()
+        rs = jnp.roll(ls, -1)
+        slopes = jnp.zeros(ls.shape)
+        w = jnp.where(ls * rs > 0.0)
+        slopes[w] = 2.0 * ls[w] * rs[w] / (ls[w] + rs[w])
 
-# Centered derivative; e.g. no slope limiter. This is unstable!
-@jit
-def Cen(f):
-    ls = left_slope(f)  # left slope
-    rs = jnp.roll(ls, -1)  # roll down once (giving the slope to the right)
-    return 0.5 * (
-        ls + rs
-    )  # the average of the left and right slopes is simply the centered derivative (deriv(f))
+        return (slopes,)
+    else:
+        shape = jnp.insert(jnp.array(function_values.shape), 0, function_values.ndim)
+        slopes = jnp.zeros(shape)
 
+        for i in range(function_values.ndim):
+            ls = left_slope(function_values, axis=i)
+            rs = jnp.roll(ls, -1, axis=i)
+            cs = jnp.zeros(function_values.shape)
+            w = jnp.where(ls * rs > 0.0)
+            cs[w] = 2.0 * ls[w] * rs[w] / (ls[w] + rs[w])
+            slopes[i] = cs
 
-# MinMod slope limiter
-@jit
-def MinMod(f):
-    ls = left_slope(f)  # left slope
-    rs = jnp.roll(ls, -1)  # right slope
-    sign = jnp.ones(ls.shape)  # start with making sign equal +1
-    sign[jnp.where(ls < 0.0)] -= 1.0  # where left slope is negative, make sign 0
-    sign[
-        jnp.where(rs < 0.0)
-    ] -= 1.0  # if left *and* right slope are negative, sign is -1, if only right slope is negative, sign is 0
-    return (
-        jnp.minimum(jnp.abs(ls), jnp.abs(rs)) * sign
-    )  # return the smallest size of the two slopes with correct sign, but zero if they disagree about sign
-
-
-# MonCen slope limiter
-@jit
-def MonCen(f):
-    size = jnp.shape(f)[0]
-
-    ls = left_slope(f)  # left slope
-    rs = jnp.roll(ls, -1)  # right slope
-    cs = jnp.zeros(size)  # MonCen starts out as zero slope
-    w = jnp.where(
-        ls * rs > 0.0, size=size
-    )  # Where both slopes agree in sign, we compute it
-    cs = cs.at[w].set(
-        2.0 * ls[w] * rs[w] / (ls[w] + rs[w])
-    )  # MonCen slope is the harmonic average of the left and right state
-    return cs
+        return (slopes,)
 
 
 # Define an hydro state including all auxiliary scalars and coordinates in a class
 class HydroState(object):
-    def __init__(self, n, gamma=1.0, cs=1.0, Lbox=2.0 * jnp.pi):
-        dx, x = coordinates(n, Lbox=Lbox)
-        self.n = n  # number of points
-        self.dx = dx  # cell size
-        self.Lbox = Lbox  # Box size
-        self.x = x  # coordinate axis
-        self.gamma = gamma  # adiabatic index
-        self.cs = cs  # initial sound speed, if isothermal it is fixed
-        self.t = 0.0  # time
-        self.rho = jnp.ones(n)  # density
-        self.Px = jnp.zeros(n)  # momentum density = rho*velocity_x
-        if gamma != 1.0:  # non-isothermal
-            Pressure = cs**2 * self.rho / gamma
-            Eint = Pressure / (self.gamma - 1.0)
-            self.Etot = Eint  # total energy
+    def __init__(self, n=64, adiabatic_index=1.4, sound_speed=1.0, Lbox=2.0):
+        self.n = n
+        self.Lbox = Lbox
+        self.time = 0.0
+        self.iterations = 0
+        self.adiabatic_index = adiabatic_index
+        if adiabatic_index == 1.0:
+            self.sound_speed = sound_speed
+        self.density = jnp.ones((n, n))
+        self.total_energy = jnp.ones((n, n))
+        self.Px = jnp.zeros((n, n))
+        self.Py = jnp.zeros((n, n))
+        self._initialize_coordinates()
 
-    # Compute velocity from state vector
+    def _initialize_coordinates(self):
+        n = self.n
+
+        self.ds = self.Lbox / n
+        self.dx = self.ds
+        self.dy = self.ds
+
+        # cell-centered coordinates
+        self.x_coords = (
+            jnp.linspace(-self.Lbox * 0.5, self.Lbox * 0.5, num=self.n, endpoint=False)
+            + 0.5 * self.ds
+        )
+        self.y_coords = self.x_coords
+
+        self.radius = jnp.sqrt(self.y_coords**2 + self.x_coords**2)
+
+    @property
     def velocity(self):
         """Compute velocity from conservative variables"""
-        return self.Px / self.rho
+        return jnp.array([self.Px / self.density, self.Py / self.density])
 
-    # Compute pressure from state vector
+    @property
     def pressure(self):
         """Compute pressure from conservative variables"""
-        if self.gamma == 1.0:
-            P = self.cs**2 * self.rho
+        if self.adiabatic_index == 1.0:
+            pressure = self.sound_speed**2 * self.density
         else:
-            Eint = self.Etot - 0.5 * self.Px**2 / self.rho  # Internal energy
-            P = (self.gamma - 1.0) * Eint
-        return P
+            internal_energy = self.total_energy - 0.5 * self.Px**2 / self.density
+            pressure = (self.adiabatic_index - 1.0) * internal_energy
+        return pressure
 
-    def sound_speed(self):
-        """Sound speed for HD"""
-        # if gamma=1 gas is isothermal, and sound speed is a property of the equation of state
-        if self.gamma == 1.0:
-            return self.cs
-        else:
-            P = self.pressure()
-            cs = jnp.sqrt(self.gamma * P / self.rho)
-            return cs
+    @property
+    def temperature(self):
+        """Compute the temperature, defined as  P/rho = kB / mu T"""
+        return self.pressure / self.density
 
     # Courant condition with default Courant number=0.2 for a fluid
     # maximum propagation velocity is max(|v| + sound speed), where max is taken over all cells
-    def Courant(self, Cdt=0.2):
+    def courant_condition(self, courant_number=0.2):
         """Courant condition for HD"""
-        speed = abs(self.velocity())
-        dt = Cdt * self.dx / jnp.max(speed + self.sound_speed())
-        return dt
+        v_x, v_y = self.velocity
+        speed = jnp.sqrt(v_x**2 + v_y**2)
+        sound_speed = jnp.sqrt(self.adiabatic_index * self.pressure / self.density)
+        return courant_number * self.ds / jnp.max(sound_speed + speed)
+
 
 @chex.dataclass
 class Primitive:
     D: Array
     U: Array
+    V: Array
     P: Array
     gamma: float = 1.0
 
@@ -137,27 +117,35 @@ class Primitive:
 class Conservative:
     D: Array
     mU: Array
+    mV: Array
+    Etot: Optional[Array]
 
 
-# Conservative variables computed from primitve variable
 @jit
 def primitive_to_conservative(q: Primitive) -> Conservative:
-    return Conservative(D=q.D, mU=q.D * q.U)
+    """Conservative variable from primitive variable"""
+    total_energy = (
+        q.P / (q.gamma - 1) + 0.5 * q.D * (q.U**2 + q.V**2)
+        if q.gamma != 1
+        else None
+    )
+
+    return Conservative(D=q.D, mU=q.D * q.U, mV=q.D * q.V, Etot=total_energy)
 
 
-# Hydro flux from conservative and primitive variables
 @jit
 def hydro_flux(q, U):
-    return Conservative(D=U.mU, mU=U.mU * q.U + q.P)
+    """Flux from conservative and primitive variables"""
+    total_energy = (U.Etot + q.P) * q.U if q.gamma != 1 else None
+
+    return Conservative(D=U.mU, mU=U.mU * q.U + q.P, mV=U.mV * q.U, Etot=total_energy)
 
 
-# LLF is the most diffuse Riemann solver. But also the most stable.
-# ql = (density, velocity, pressure) = (D, U, P), qr are state vectors for the _primitive_ variables
 @jit
-def LLF(ql, qr):
+def LLF(ql: Primitive, qr: Primitive) -> Conservative:
     # sound speed for each side of interface (l==left, r==right)
-    c_left = (ql.gamma * ql.P / ql.D) ** 0.5
-    c_right = (qr.gamma * qr.P / qr.D) ** 0.5
+    c_left = jnp.sqrt(ql.gamma * ql.P / ql.D)
+    c_right = jnp.sqrt(qr.gamma * qr.P / qr.D)
     c_max = jnp.maximum(c_left, c_right)
 
     # maximum absolute wave speed for left and right state
@@ -168,103 +156,109 @@ def LLF(ql, qr):
     Ur = primitive_to_conservative(qr)
 
     # Hydro fluxes
-    Fl = hydro_flux(ql, Ul)
-    Fr = hydro_flux(qr, Ur)
+    flux_left = hydro_flux(ql, Ul)
+    flux_right = hydro_flux(qr, Ur)
 
-    # LLF flux based on maximum wavespeed.
-    # The general form is "(F_left + F_right - cmax*(U_right - U_left)) / 2"
-    # where U is the state vector of the conserved variables
-    flux_D = 0.5 * (Fl.D + Fr.D - cmax * (Ur.D - Ul.D))
-    flux_mU = 0.5 * (Fl.mU + Fr.mU - cmax * (Ur.mU - Ul.mU))
-    # Flux.Etot = ...
+    # update conservative variable
+    flux_D = jnp.sqrt(flux_left.D + flux_right.D - cmax * (Ur.D - Ul.D))
+    flux_mU = jnp.sqrt(flux_left.mU + flux_right.mU - cmax * (Ur.mU - Ul.mU))
+    flux_mV = jnp.sqrt(flux_left.mV + flux_right.mV - cmax * (Ur.mV - Ul.mV))
+    flux_Etot = (
+        jnp.sqrt(flux_left.Etot + flux_right.Etot - cmax * (Ur.Etot - Ul.Etot))
+        if ql.gamma != 1
+        else None
+    )
 
-    return Conservative(D=flux_D, mU=flux_mU)
-
-
-# HLL (Harten, Lax, van Leer) is a bit less diffuse. We compute individual wave speeds for each state
-# ql, qr are state vectors for the _primitive_ variables
-@jit
-def HLL(ql, qr):
-    # sound speed for each side of interface (l==left, r==right)
-    c_left = (ql.gamma * ql.P / ql.D) ** 0.5
-    c_right = (qr.gamma * qr.P / qr.D) ** 0.5
-    c_max = jnp.maximum(c_left, c_right)
-
-    # maximum wave speeds to the left and right (guaranteed to have right sign)
-    SL = jnp.minimum(jnp.minimum(ql.U, qr.U) - c_max, 0)  # <= 0.
-    SR = jnp.maximum(jnp.maximum(ql.U, qr.U) + c_max, 0)  # >= 0.
-
-    # Hydro conservative variable
-    Ul = primitive_to_conservative(ql)
-    Ur = primitive_to_conservative(qr)
-
-    # Hydro fluxes
-    Fl = hydro_flux(ql, Ul)
-    Fr = hydro_flux(qr, Ur)
-
-    # HLL flux based on wavespeeds. If SL < 0 and SR > 0 then mix state appropriately
-    # The general form is
-    #    (SR * F_left - SL * F_right + SL * SR *(U_right - U_left)) / (SR - SL)
-    # where U is the state vector of the conserved variables
-    flux_D = (SR * Fl.D - SL * Fr.D + SL * SR * (Ur.D - Ul.D)) / (SR - SL)
-    flux_mU = (SR * Fl.mU - SL * Fr.mU + SL * SR * (Ur.mU - Ul.mU)) / (SR - SL)
-    # Flux.Etot = ...
-
-    return Conservative(D=flux_D, mU=flux_mU)
+    return Conservative(D=flux_D, mU=flux_mU, mV=flux_mV, Etot=flux_Etot)
 
 
-# Hydrodynamics solver based on MUSCL scheme
-def muscl(u: HydroState, dt, Slope=MinMod, Riemann_Solver=HLL):
-    dx = u.dx
-    idx = 1.0 / u.dx
-    dtdx = dt / u.dx
+def muscl_2d(state: HydroState, dt, slope_func=monotonized_central, solver_func=LLF):
+    ds = state.ds
+    inv_ds = 1.0 / ds
+    dt_ds = dt / ds
 
-    # 1) Compute primitive variables at cell center (rho, v, P)
-    rho = u.rho
-    v = u.velocity()
-    P = u.pressure()
+    # primitive variables
+    density = jnp.copy(state.density)
+    velocity = state.velocity
+    pressure = state.pressure
 
-    # 2) Compute slope limited derivatives based on centered points
-    drhodx = Slope(rho) * idx
-    dvdx = Slope(v) * idx
-    dPdx = Slope(P) * idx
+    # slope limited derivatives
+    d_density = slope_func(density) * inv_ds
+    d_pressure = slope_func(pressure) * inv_ds
+    d_vx = slope_func(velocity[0]) * inv_ds
+    d_vy = slope_func(velocity[1]) * inv_ds
+    d_velocity = jnp.array([d_vx, d_vy])
 
-    # 3) Trace forward to find solution at [t+dt/2, x +- dx/2]
-    # Time evolution for source terms
-    rho_t = -v * drhodx - dvdx * rho
-    v_t = -v * dvdx - dPdx / rho
-    # Here we need to compute time evolution for P
+    divergence = d_velocity[0, 0] + d_velocity[1, 1]
+    density_t = (
+        -velocity[0] * d_density[0] - velocity[1] * d_density[1] - density * divergence
+    )
+    pressure_t = (
+        -velocity[0] * d_pressure[0]
+        - velocity[1] * d_pressure[1]
+        - state.adiabatic_index * pressure * divergence
+    )
+    velocity_t = jnp.empty_like(velocity)
+    velocity_t[0] = (
+        -velocity[0] * d_vx[0] - velocity[1] * d_vx[1] - d_pressure[0] / density
+    )
+    velocity_t[1] = (
+        -velocity[0] * d_vy[0] - velocity[1] * d_vy[1] - d_pressure[1] / density
+    )
 
-    # Spatial interpolation + time terms
-    # left state at t + dt/2 -- AS SEEN FROM THE INTERFACE
-    ql_D = rho + 0.5 * (dt * rho_t + dx * drhodx)
-    ql_U = v + 0.5 * (dt * v_t + dx * dvdx)
-    ql_P = u.cs**2 * ql_D
-    ql = Primitive(D=ql_D, U=ql_U, P=ql_P, gamma=u.gamma)
+    for axis in range(2):
+        adiabatic_index = state.adiabatic_index
 
-    # Spatial interpolation + time terms
-    # right state at t + dt/2 -- AS SEEN FROM THE INTERFACE
-    qr_D = rho + 0.5 * (dt * rho_t - dx * drhodx)
-    qr_U = v + 0.5 * (dt * v_t - dx * dvdx)
-    qr_P = u.cs**2 * qr_D
+        # Calculates the parallel and perpendicular velocities to the coordinate direction
+        # X-axis: iu=0, iv=1, Y-axis: iu=1, iv=0
+        iu = axis
+        iv = (axis + 1) % 2
+        # Parallel (U) and perpendicular (V) velocities
+        U = velocity[iu]
+        dU = d_velocity[iu]
+        U_t = velocity_t[iu]
 
-    qr = Primitive(D=qr_D, U=qr_U, P=qr_P, gamma=u.gamma)
+        V = velocity[iv]
+        dV = d_velocity[iv]
+        V_t = velocity_t[iv]
 
-    # make sure that right state is centered correctly.
-    # Numerical index follow upstaggered interfaces,
-    qr.D = jnp.roll(qr.D, -1)
-    qr.U = jnp.roll(qr.U, -1)
-    qr.P = jnp.roll(qr.P, -1)
+        # left state
+        density_left = density + 0.5 * (dt * density_t + ds * d_density[axis])
+        pressure_left = pressure + 0.5 * (dt * pressure_t + ds * d_pressure[axis])
+        U_left = U + 0.5 * (dt * U_t + ds * dU[axis])
+        V_left = V + 0.5 * (dt * V_t + ds * dV[axis])
 
-    # 4) Solve for flux based on interface values
-    Flux = Riemann_Solver(ql, qr)
+        state_left = Primitive(D=density_left, U=U_left, V=V_left, P=pressure_left)
 
-    # 5) Update conserved variables.
-    #    From the cell center (at x_i) point of view:
-    #       * 1st term is the upstaggered value at the interface position x_i + dx/2
-    #       * 2nd term is the downstaggered value at the interface position x_i - dx/2. Therefore we roll.
-    u.rho += -dtdx * (Flux.D - jnp.roll(Flux.D, 1))
-    u.Px += -dtdx * (Flux.mU - jnp.roll(Flux.mU, 1))
-    # Update energy flux ...
+        # right state
+        density_right = density + 0.5 * (dt * density_t - ds * d_density[axis])
+        pressure_right = pressure + 0.5 * (dt * pressure_t - ds * d_pressure[axis])
+        U_right = U + 0.5 * (dt * U_t - ds * dU[axis])
+        V_right = V + 0.5 * (dt * V_t - ds * dV[axis])
 
-    return u
+        for var in (density_left, pressure_left, U_left, V_left):
+            var = jnp.roll(var, -1, axis=axis)
+
+        state_right = Primitive(D=density_right, U=U_right, V=V_right, P=pressure_right)
+
+
+        # solve for flux
+        flux = solver_func(state_left, state_right)
+
+        # 7) Update conserved variables with fluxes
+        state.density -= dt_ds * (flux.D - jnp.roll(flux.D, 1, axis=axis))
+
+        if state.adiabatic_index != 1:
+            state.total_energy -= dt_ds * (flux.Etot - jnp.roll(flux.Etot, 1, axis=axis))
+
+        if axis == 0:
+            state.Px -= dt_ds * (flux.mU - jnp.roll(flux.mU, 1, axis=axis))
+            state.Py -= dt_ds * (flux.mV - jnp.roll(flux.mV, 1, axis=axis))
+        elif axis == 1:
+            state.Px -= dt_ds * (flux.mV - jnp.roll(flux.mV, 1, axis=axis))
+            state.Py -= dt_ds * (flux.mU - jnp.roll(flux.mU, 1, axis=axis))
+
+    state.time += dt
+    state.iterations += 1
+
+    return state
